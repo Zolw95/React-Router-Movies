ef9780409776228fc6bd7d7cf28135082f6c58de
unknown <miloszz87@gmail.com>
2020-05-10 19:25:53 -0400

    added link for rendered movies

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Movie from './Movie';
 
+
 const SavedList = props => (
 
   <div className="saved-list">
 
     <h3>Saved Movies:</h3>
     {props.savedList.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <Link to={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></Link>
     ))}
     <Link to="/">
     <div className="home-button">Home</div>
